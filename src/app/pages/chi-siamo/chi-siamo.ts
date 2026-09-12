500c55a4ec80c59e3bef853875263b4fa7c5106d
darioeliaaa <darioelia61@gmail.com>
2026-09-12 12:21:19 +0200

    a

--- src/app/pages/chi-siamo/chi-siamo.ts
+++ src/app/pages/chi-siamo/chi-siamo.ts
@@ -12,7 +12,7 @@ export class ChiSiamo {
   sottotitolo = "Da generazioni coltiviamo la terra di Strongoli con passione e rispetto.";
 
   storia = [
-    "L'Azienda Agricola Pelaggi nasce dal profondo legame con il territorio calabrese. Da decenni ci dedichiamo alla cura dei nostri uliveti, tramandando di padre in figlio le tecniche di potatura e raccolta.",
+    "L'Azienda Agricola nasce dal profondo legame con il territorio calabrese. Da decenni ci dedichiamo alla cura dei nostri uliveti, tramandando di padre in figlio le tecniche di potatura e raccolta.",
     "Non siamo solo agricoltori, ma anche artigiani dell'olio. Avere un frantoio di proprietà ci permette di controllare ogni singola goccia che finisce in bottiglia, garantendo un prodotto che è l'essenza pura della nostra terra."
   ];
 }
